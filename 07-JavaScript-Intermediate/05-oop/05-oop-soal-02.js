567cf0aefed739fc86b2453e4c0ba1579cde7401
deviayulestari <55142624+deviayulestari@users.noreply.github.com>
2021-10-09 21:54:21 +0700

    add 05-oop-soal-02.js

---
+++ 07-JavaScript-Intermediate/05-oop/05-oop-soal-02.js
@@ -0,0 +1,24 @@
+// 1. Apa itu Encapsulation? Mengapa kita membutuhkannya?
+// Encapsulation adalah cara untuk membatasi akses langsung ke properti atau method dari sebuah objek.
+// Kita membutuhkannya agar properti atau method tetap terjaga sehingga tidak adanya penyalahgunaan.
+// Encapsulation juga sering digunakan karena terdapat fitur information-hiding mechanism. Mekanisme ini 
+// menghilangkan akses publik ke atribut-atribut yang terdapat di dalam “kapsul” tersebut sehingga dapat memudahkan 
+// dalam mendefinisikan atribut apa saja yang dapat dibaca dan diperbarui sehingga dapat mencegah perubahan yang tidak disengaja.
+
+
+// 2. Apa itu Abstraction? Mengapa kita membutuhkannya?
+// Abstraction adalah sebuah teknik untuk menyembunyikan detail tertentu dari sebuah objek/method 
+// dan hanya menampilkan fungsionalitas atau fitur penting dari objek tersebut.
+// Kita membutuhkannya karena untuk menyembunyikan detail yang tidak terlalu penting sehingga lebih mudah dibaca
+// dan terorganisir apa saja fitur-fitur utama/penting yang dibangun.
+
+// 3. Apa itu Inheritance? Mengapa kita membutuhkannya?
+// Inheritance dalam OOP adalah sebuah proses dimana sebuah class mewariskan property dan methodnya ke class lain atau childnya.
+// Kita membutuhkannya karena kita dapat membuat sub-class(child) yang dapat menggunakan semua fungsi dari parent class.
+// Dengan konsep pewarisan, kita dapat menggunakan kode yang telah ditulis pada kelas parent class berulang kali pada kelas-kelas turunannya 
+// tanpa harus menulis ulang semua kode-kode itu.
+
+// 4. Apa itu Polymorpishm? Mengapa kita membutuhkannya?
+// Polymorpish pada konsep OOP merupakan kemampuan dari suatu objek untuk memiliki banyak bentuk.
+// Kita membutuhkannya karena dengan polymorphism fungsi yang sama dapat digunakan pada tipe yang berbeda sehingga membuat programming lebih intuitif dan mudah.
+// Polymorphism juga mengizinkan menerapkan beberapa implementasi method pada kelas yang sama namun dengan parameter yang berbeda-beda.
